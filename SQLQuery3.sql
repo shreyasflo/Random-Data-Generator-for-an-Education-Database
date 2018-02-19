cca41f26083740f9ac51b4cce8143c00d708329d
Shreyas Ramnath <shreyasflo@gmail.com>
2018-02-19 15:09:14 -0500

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,3 +0,0 @@
-Select count (*) as num_instructors 
-from department as d1, instructor as i, department as d2
-where d1.department_name = i.department_name and d2.department_name=’MTH’ and d2.building = d1.building;
