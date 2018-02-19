7fa7a1008ac261ec8bd16e7b52ae0c4a251786a4
Shreyas Ramnath <shreyasflo@gmail.com>
2018-02-19 15:09:05 -0500

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,3 +0,0 @@
-Select p.i_id , p.name, p.department_name, p.salary 
-from instructor as p,instructor as q 
-where p.department_name = q.department_name group by q.department_name having p.salary < (0.35*max(q.salary));
