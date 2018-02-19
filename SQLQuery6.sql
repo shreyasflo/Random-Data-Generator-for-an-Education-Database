6e671690c603ea1b8cc75a6f1d8a352d79929f4e
Shreyas Ramnath <shreyasflo@gmail.com>
2018-02-19 15:08:46 -0500

    Delete SQLQuery6.sql

--- SQLQuery6.sql
+++
@@ -1,7 +0,0 @@
-select i_id 
-from teaches 
-where sec_id in(select sec_id 
-				from section 
-				where building in(select building 
-								  from department 
-								  where department_name = ’CHM’));
